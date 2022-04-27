64a2cba77e73039ae304800e72dab7ab1d9248ab
HYTan0909 <87137966+HYTan0909@users.noreply.github.com>
2022-04-27 13:05:51 +0800

    Add files via upload

---
+++ trip_collection.R
@@ -0,0 +1,6 @@
+install.packages("mongolite")
+
+library(mongolite)
+# This is the connection_string. You can get the exact url from your MongoDB cluster screen
+connection_string = 'mongodb+srv://hytan:1234@MyCluster1.ihzso.mongodb.net/sample_training'
+trips_collection = mongo(collection="trips", db="sample_training", url=connection_string)
