f3e3b233962c356a79a7ea87c3c0460d97235f63
HYTan0909 <87137966+HYTan0909@users.noreply.github.com>
2022-05-18 13:01:23 +0800

    Add files via upload

---
+++ AA/HashTable q2.py
@@ -0,0 +1,49 @@
+class HashNode:
+  def __init__ (self,key,data):
+    self.key = key
+    self.data = data
+
+class HashTable:
+    def __init__(self,size):
+        self.__capacity = size
+        self.__Table = []
+        for i in range(self.__capacity):
+            self.__Table.append([])
+
+
+    def __hashFunction(self,key):
+        return key % self.__capacity;
+
+    def insertItem(self, key, data):
+        index = self.__hashFunction(key)
+        node = HashNode(key,data)
+
+        for i in range(self.__capacity):
+            if self.__Table[i].key == key:
+                print("Index exist. ")
+            else:
+                node = HashNode(key,data)
+                self.__Table[index].append(node)
+
+
+
+    def deleteItem(self, key):
+        index = self.__hashFunction(key)
+        for i in range(len(self.__Table[index])):
+            if self.__Table[index][i].key == key:
+                self.__Table[index].pop(i)
+                break
+
+    def displayHashTable(self):
+
+        for i in range(self.__capacity):
+             print(f'table[{i}]',end='')
+             for x in self.__Table[i]:
+                print(f'-->{x.key},{x.data}',end='')
+             print()
+
+
+h= HashTable(20)
+h.insertItem(10,999)
+h.insertItem(50,888)
+h.displayHashTable()
